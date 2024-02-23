5912aebc3a419aa425481a94f620ac85f47dc65b
dacanizares <dacanizares@outlook.com>
2024-02-23 11:39:09 -0500

    Format fixes

--- main.cpp
+++ main.cpp
@@ -1,57 +1,50 @@
 #ifdef __MINGW32__
-#include <SDL.h>
+ #include <SDL.h>
 #else
-#include <SDL2/SDL.h>
+ #include <SDL2/SDL.h>
 #endif
 #include <stdio.h>
 
-// Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* args[])
 {
-	// Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO ) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		// Create window
-		SDL_Window* window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			// The surface contained by the window
-			SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
-			SDL_Event windowEvent;
-			SDL_Rect rect = {0, 0, 50, 50};
-			while (true) 
-			{
-				rect.x = (rect.x + 1) % SCREEN_WIDTH;
-				rect.y = (rect.y + 1) % SCREEN_HEIGHT;
+    if (SDL_Init(SDL_INIT_VIDEO ) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+        SDL_Window* window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == 0)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        }
+        else
+        {
+            SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+            SDL_Event windowEvent;
+            SDL_Rect rect = {0, 0, 50, 50};
+            while (1) 
+            {
+                rect.x = (rect.x + 1) % SCREEN_WIDTH;
+                rect.y = (rect.y + 1) % SCREEN_HEIGHT;
 
-				SDL_FillRect(screenSurface, NULL, SDL_MapRGB( screenSurface->format, 255, 255, 255));
-				SDL_FillRect(screenSurface, &rect, SDL_MapRGB( screenSurface->format, 0, 0, 255));
-				SDL_UpdateWindowSurface(window);
+                SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 255, 255, 255));
+                SDL_FillRect(screenSurface, &rect, SDL_MapRGB(screenSurface->format, 0, 0, 255));
+                SDL_UpdateWindowSurface(window);
 
-				if (SDL_PollEvent(&windowEvent))
-				{
-					 if (windowEvent.type == SDL_QUIT )
-					 	break;
-				} 
-			}	
-		}
-		// Destroy window
-		SDL_DestroyWindow(window);
-	}
+                if (SDL_PollEvent(&windowEvent))
+                {
+                    if (windowEvent.type == SDL_QUIT)  break;
+                } 
+            }	
+        }
+        SDL_DestroyWindow(window);
+    }
 
-	//Quit SDL subsystems
-	SDL_Quit();
+    SDL_Quit();
 
-	return 0;
+    return 0;
 }
