400483bdf754e163201d5d956dc5476d8e5f81ba
dacanizares <dacanizares@outlook.com>
2024-02-24 20:23:07 -0500

    Updated with additional sdl libs

--- build-mac.sh
+++ build-mac.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p build/mac
-g++ main.cpp -g -ggdb -w -lSDL2 -lSDL2_image -o build/mac/main
+g++ main.cpp -g -ggdb -w -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf -o build/mac/main
