7dd88327f6f982a886e1fc7aa99e60763f06f0bf
Ron Ben-Ishay <ron.b.ishay@gmail.com>
2024-08-23 21:51:21 +0300

    Add a copy command for resources in build scripts

--- build-win.bat
+++ build-win.bat
@@ -1,3 +1,4 @@
 @echo off
 mkdir "build/win" 2>NUL
+xcopy /E /I "res" "build/win/res"
 g++ main.cpp -IC:\sdl2\include\SDL2 -LC:\sdl2\lib -g -ggdb -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf -o build/win/main
