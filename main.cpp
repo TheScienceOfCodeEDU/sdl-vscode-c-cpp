f30d1fef5ae6e89293f65ba0417607927ccba49b
dacanizares <dacanizares@outlook.com>
2024-09-01 23:45:42 -0500

    Updated code for ecc
    https: //wiki.libsdl.org/SDL2/README/emscripten
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@
 
 #define UNITY_BUILD 1
 #include <stdio.h>              // IWYU pragma: keep
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
 #ifdef _WIN64
  #include <SDL.h>
  #include <SDL_image.h>
@@ -20,35 +23,50 @@
 #include "common.h"
 #include "sdl_utils.h"
 
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+
+SDL_Texture* texture;
+
+// Sprite source rectangle
+SDL_Rect srcRect = {9, 42, 15, 21};
+// Target rectangle (note that we will paint it at x4 its original size)
+SDL_Rect destRect = {0, 0, srcRect.w * 4, srcRect.h * 4};
+
+
+void
+mainloop()
+{
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, &srcRect, &destRect);
+    SDL_RenderPresent(renderer);
+
+    SDL_Event event;
+    if (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT) {
+            #ifdef __EMSCRIPTEN__
+             emscripten_cancel_main_loop();
+            #else
+             exit(0);
+            #endif
+        }
+    }
+}
+
 int
 main(int argc, char *args[])
 {
-    SDL_Window *window;
-    SDL_Renderer *renderer;
     // Init SDL without texture filtering for better pixelart results
     if (sdl_utils_Init("SDL Tutorial", &window, &renderer, 0)) 
     {
-        SDL_Texture* texture = IMG_LoadTexture(renderer, "res/characters.png");
-        
-        // Sprite source rectangle
-        SDL_Rect srcRect = {9, 42, 15, 21};
-        // Target rectangle (note that we will paint it at x4 its original size)
-        SDL_Rect destRect = {0, 0, srcRect.w * 4, srcRect.h * 4};
-        while (1)
-        {
-            SDL_RenderClear(renderer);
-            SDL_RenderCopy(renderer, texture, &srcRect, &destRect);
-            SDL_RenderPresent(renderer);
-
-            SDL_Event event;
-            if (SDL_PollEvent(&event))
-            {
-                if (event.type == SDL_QUIT) break;
-            } 
-        }
-
-        SDL_DestroyTexture(texture);		
+        texture = IMG_LoadTexture(renderer, "./res/characters.png");
+        #ifdef __EMSCRIPTEN__
+         emscripten_set_main_loop(mainloop, 0, 1);
+        #else
+         while (1) { mainloop(); }
+        #endif
     }
-    sdl_utils_Quit(window, renderer);
     return 0;
 }
